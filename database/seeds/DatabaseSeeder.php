51ad25d961a1167101242fe2338b0e77bdf25e1a
Your Name <lirtos.get@gmail.com>
2018-01-18 21:32:29 +0200

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         User::create(
             [
                 'name' => 'GreatAdmin',
-                'email' => 'admin@la.fr',
+                'email' => 'admin',
                 'password' => bcrypt('admin'),
                 'role' => 'admin',
                 'valid' => true,
@@ -27,84 +27,6 @@ class DatabaseSeeder extends Seeder
                 'remember_token' => str_random(10),
             ]
         );
-        User::create(
-            [
-                'name' => 'GreatRedactor',
-                'email' => 'redac@la.fr',
-                'password' => bcrypt('redac'),
-                'role' => 'redac',
-                'valid' => true,
-                'confirmed' => true,
-                'remember_token' => str_random(10),
-            ]
-        );
-        User::create(
-            [
-                'name' => 'Walker',
-                'email' => 'walker@la.fr',
-                'password' => bcrypt('walker'),
-                'role' => 'user',
-                'valid' => true,
-                'confirmed' => true,
-                'remember_token' => str_random(10),
-            ]
-        );
-        User::create(
-            [
-                'name' => 'Slacker',
-                'email' => 'slacker@la.fr',
-                'password' => bcrypt('slacker'),
-                'role' => 'user',
-                'valid' => true,
-                'confirmed' => true,
-                'remember_token' => str_random(10),
-            ]
-        );
-        User::create(
-            [
-                'name' => 'Worker',
-                'email' => 'worker@la.fr',
-                'password' => bcrypt('worker'),
-                'role' => 'user',
-                'valid' => false,
-                'confirmed' => true,
-                'remember_token' => str_random(10),
-            ]
-        );
-
-        // Uncheck new for these users
-        foreach(User::all() as $user) {
-            $user->ingoing->delete();
-        }
-
-        $nbrUsers = 5;
-
-        // Other users
-        factory(User::class, 15)->create();
-        sleep(2);
-        User::create(
-            [
-                'name' => 'Sorditofublos',
-                'email' => 'sordi@la.fr',
-                'password' => bcrypt('sordi'),
-                'role' => 'user',
-                'valid' => true,
-                'confirmed' => true,
-                'remember_token' => str_random(10),
-            ]
-        );
-        $user = User::create(
-            [
-                'name' => 'Martinobinus',
-                'email' => 'martin@la.fr',
-                'password' => bcrypt('martin'),
-                'role' => 'user',
-                'valid' => false,
-                'confirmed' => false,
-                'remember_token' => str_random(10),
-            ]
-        );
-        $user->ingoing->delete();
 
         // Categories
         DB::table('categories')->insert([
@@ -121,16 +43,6 @@ class DatabaseSeeder extends Seeder
                 'slug' => 'category-3'
             ],
         ]);
-
-        $nbrCategories = 3;
-
-        // Contacts
-        factory(Contact::class, 5)->create();
-        sleep(2);
-        factory(Contact::class)->create([
-            'name' => 'Softagonopoulos',
-        ]);
-
 
         // Tags
         DB::table('tags')->insert([
